ba90a5b40f0a15f60556f4201c201e01c524e518
lfravega <leo-fravega@hotmail.com>
2025-10-12 20:45:40 -0300

    feat: Seteo de modulos user y starwars a la api. Implementacion de Jwt, modulo para consultar api externa, passport. Configuracion docker y paquetes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGODB_URI } from './shared/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { MONGODB_URI, JWT_SECRET, JWT_EXPIRES_IN } from './shared/config';
 import { UsersModule } from './users/users.module';
+import { StarwarsModule } from './starwars/starwars.module';
+import { JwtStrategy } from './common/localstrategy';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     MongooseModule.forRoot(MONGODB_URI || ''),
-    UsersModule
+    HttpModule,
+    PassportModule,
+    JwtModule.register({
+      secret: JWT_SECRET || 'default-secret-key',
+      signOptions: { expiresIn: JWT_EXPIRES_IN as any || '24h' },
+    }),
+    UsersModule,
+    StarwarsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {}
