78fa9ad2a9c897f930bde88e7dd38a0141e002da
lfravega <leo-fravega@hotmail.com>
2025-10-14 08:10:49 -0300

    changes: Se cambia el mensaje al hacer get a la raiz de la api.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '¡Hola Mundo!';
+    return 'Api de StarWars. Accede a la documentación en /api/docs';
   }
 }
